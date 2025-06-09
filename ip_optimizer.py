631b280793c2174f8eb62f503280da2eea00649f
JasonV001 <15041669699@139.com>
2025-06-09 11:40:42 +0800

    更新 ip_optimizer.py

--- ip_optimizer.py
+++ ip_optimizer.py
@@ -24,8 +24,8 @@ CONFIG = {
     "RTT_RANGE": "10~2000",          # 延迟范围(ms)
     "LOSS_MAX": 30.0,               # 最大丢包率(%)
     "THREADS": 50,                  # 并发线程数
-    "IP_POOL_SIZE": 100000,          # IP池总大小 [新增]
-    "TEST_IP_COUNT": 500,          # 实际测试IP数量 [新增]
+    "IP_POOL_SIZE": 20000,          # IP池总大小 [新增]
+    "TEST_IP_COUNT": 800,          # 实际测试IP数量 [新增]
     "TOP_IPS_LIMIT": 15,            # 精选IP数量
     "CLOUDFLARE_IPS_URL": "https://www.cloudflare.com/ips-v4",
     "CUSTOM_IPS_FILE": "custom_ips.txt",          # 自定义IP池文件路径
